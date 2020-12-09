e74a08715bb429b36a154fb9b9dc98724f7a7a87
phuocnh3 <phuocnh@alive.vn>
2020-12-09 21:11:46 +0700

    fix db connection

--- server/bus/user.js
+++ server/bus/user.js
@@ -2,8 +2,8 @@ const db = require("../utils/db");
 const utils = require("../utils/utils");
 
 const TABLE = "users";
-const viewFields = [];
-const changeFields = [];
+const viewFields = ["id","username","name"];
+const changeFields = ["id","username","name"];
 
 module.exports = {
   ...utils.commonModel(db, TABLE, viewFields, changeFields),
