2b38ace9f08a9f898789135e410e274981e78d0d
phuocnh3 <phuocnh@alive.vn>
2020-12-30 22:40:18 +0700

    Merge from Dev_Phuong into master

--- client/user/src/component/home/listUser.js
+++ client/user/src/component/home/listUser.js
@@ -1,27 +1,8 @@
 import React, { useState, useEffect } from "react";
 import Item from "./itemUser";
 
-function listUser({ listUsers }) {
-  const listUser = [
-    { username: "Hồ Mình Khai", status: 1 },
-    { username: "Trần Hoài An", status: 0 },
-    { username: "Nguyễn Phương Linh", status: 1 },
-    { username: "Hồ Mình Khai", status: 1 },
-    { username: "Trần Hoài An", status: 1 },
-    { username: "Nguyễn Phương Linh", status: 1 },
-    { username: "Hồ Mình Khai", status: 1 },
-    { username: "Trần Hoài An", status: 1 },
-    { username: "Nguyễn Phương Linh", status: 1 },
-    { username: "Hồ Mình Khai", status: 1 },
-    { username: "Trần Hoài An", status: 1 },
-    { username: "Nguyễn Phương Linh", status: 1 },
-    { username: "Hồ Mình Khai", status: 1 },
-    { username: "Trần Hoài An", status: 1 },
-    { username: "Nguyễn Phương Linh", status: 1 },
-    { username: "Hồ Mình Khai", status: 1 },
-    { username: "Trần Hoài An", status: 1 },
-    { username: "Nguyễn Phương Linh", status: 1 },
-  ];
+function listUser({ listUser }) {
+  console.log(listUser)
   return (
     <>
       <div>
