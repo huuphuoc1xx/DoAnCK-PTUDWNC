8276b258fa7ef15fe2bcd431767a22dec3e8e4a9
phuocnh3 <phuocnh@alive.vn>
2021-01-13 07:26:36 +0700

    add get list user, history

--- server/models/game.js
+++ server/models/game.js
@@ -1,25 +1,45 @@
 const db = require('../utils/db');
 
 const startGame = ({ username_x }) => {
-    const entity = {
-        player_x: username_x,
-        player_o: null,
-        detail: null,
-        result: null
-    }
-    return db.add('game', entity).then(res => res.insertId);
+  const entity = {
+    player_x: username_x,
+    player_o: null,
+    detail: null,
+    result: null
+  }
+  return db.add('game', entity).then(res => res.insertId);
 }
 const updateGame = (entity, id) => db.edit('game', entity, { id });
 const getGame = (room) => db.load(`SELECT detail FROM game WHERE id =?`, [room]).then(res => res[0]);
 const updateUserPlay = (fieldChange, data, id) => {
-    const entity = {}
-    entity[fieldChange] = data;
-    return db.edit('game', entity, { id: id });
+  const entity = {}
+  entity[fieldChange] = data;
+  return db.edit('game', entity, { id: id });
+}
+
+const filterGame = ({ id, player_o, player_x, last_id, page_size }) => {
+  const condition = ["TRUE"];
+  const params = [];
+  const filterFields = { id, player_o, player_x }
+  Object.keys(filterFields).filter(item => filterFields[item]).forEach(item => {
+    condition.push(`${item} = ?`);
+    params.push(filterFields[item]);
+  });
+
+  if (last_id > 0) {
+    condition.push("id < ?");
+    params.push(last_id);
+  }
+  return db.load(
+    `SELECT id, player_x, player_o, IF(result="X",player_x, IF(result="O",player_o, NULL)) winner FROM game
+      WHERE ${condition.join(" AND ")} 
+      ORDER BY id DESC 
+      LIMIT ${Math.max(+page_size || 0, 10)}`, params);
 }
 module.exports = {
-    startGame,
-    updateGame,
-    getGame,
-    updateUserPlay
-
+  startGame,
+  updateGame,
+  getGame,
+  updateUserPlay,
+  filterGame
 }
--- server/routes/admin/login.js
+++ server/routes/admin/login.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const controller = require('../../controllers/login');
+const { ensureAuthenticated } = require('../../services/authentication');
 const router = express.Router();
 
 router.post('/', controller.login("ADMIN"));
+router.get('/validate', ensureAuthenticated("ADMIN"), (req, res) => {res.json({ code: 0, data: { user: req.user } }) });
 
 module.exports = router;
