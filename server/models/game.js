bb2c4a4d87200656ae994cbb71141cfcbdbb1d98
Phuong Nguyen <phuongntk@alive.vn>
2021-01-13 17:27:37 +0700

    chat

--- server/models/game.js
+++ server/models/game.js
@@ -63,6 +63,9 @@ const getInfor = (id) => db.load(`SELECT cup, lose, win FROM users WHERE id = ?`
 const updateCup = (userId, cup) => db.edit('users', { cup: cup }, { id: userId });
 const updateWinChess = (userId, win) => db.edit('users', { win: win }, { id: userId });
 const updateLoseChess = (userId, lose) => db.edit('users', { lose: lose }, { id: userId });
+const getListMess = (room) => db.load(`SELECT message FROM game WHERE id =?`, [room]).then(res => res[0] && res[0].message);
+
+
 module.exports = {
   startGame,
   updateGame,
