199828b55209bd198cf188384e1b0ab05f677f12
phuocnh3 <phuocnh@alive.vn>
2021-01-14 14:44:00 +0700

    fix modules

--- client/admin/src/component/History/History.js
+++ client/admin/src/component/History/History.js
@@ -1,6 +1,6 @@
 import Axios from "axios";
 import React, { useEffect, useState } from "react";
-import DataTable from "../../../../user/src/component/History/node_modules/react-data-table-component";
+import DataTable from "react-data-table-component";
 import config from "../../config/config.json";
 import { Button, Form, Pagination } from "react-bootstrap";
 import ShowDetail from "./ShowDetail";
